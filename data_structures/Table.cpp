6126c195db7e4559f7dab94268afc3aab8a61975
Jakub Plona <plonajakub@gmail.com>
2019-03-09 23:47:45 +0100

    init

---
+++ data_structures/Table.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jakub Plona PL on 09.03.2019.
+//
+
+#include "Table.h"
---
+++ data_structures/Table.h
@@ -0,0 +1,14 @@
+//
+// Created by Jakub Plona PL on 09.03.2019.
+//
+
+#ifndef SDIZO_P1_TABLE_H
+#define SDIZO_P1_TABLE_H
+
+
+class Table {
+
+};
+
+
+#endif //SDIZO_P1_TABLE_H
