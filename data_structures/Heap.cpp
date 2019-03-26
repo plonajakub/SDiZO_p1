5e68fde8cd427e816ae84995bb2f00b02e8da3d9
Jakub Plona <plonajakub@gmail.com>
2019-03-26 16:17:11 +0100

    Heap.h and Heap.cpp reformatted

--- data_structures/Heap.cpp
+++ data_structures/Heap.cpp
@@ -1,13 +1,11 @@
-//
-// Created by Jakub Plona PL on 22.03.2019.
-//
-
 #include <iomanip>
 #include "Heap.h"
 
 void Heap::insert(int key) {
     table.insertAtEnd(0);
     int keyIndex = table.getSize() - 1;
+
+    // Get higher indexes for the new key until it the place meets the heap's condition
     while (keyIndex > 0 && table[parent(keyIndex)] < key) {
         table[keyIndex] = table[parent(keyIndex)];
         keyIndex = parent(keyIndex);
@@ -16,20 +14,28 @@ void Heap::insert(int key) {
 }
 
 void Heap::remove(int key) {
+    // Get key's index
     int rmIdx = search(key);
     if (rmIdx == KEY_NOT_FOUND) {
         throw std::invalid_argument("Key does not exist");
     }
+
+    // Move last value in heap into the removed key's place
     table[rmIdx] = table[table.getSize() - 1];
     table.removeAtEnd();
+
+    // Decide which algorithm to use in order to restore heap's order
     if (rmIdx > 0 && rmIdx != table.getSize() && table[rmIdx] > table[parent(rmIdx)]) {
         int movedValueIdx = rmIdx;
+
+        // Move value up in the heap
         while (movedValueIdx > 0 && table[parent(movedValueIdx)] < table[movedValueIdx]) {
             swap(table[movedValueIdx], table[parent(movedValueIdx)]);
             movedValueIdx = parent(movedValueIdx);
         }
-    }
-    else {
+    } else {
+
+        // Move value down in the heap
         heapify(rmIdx);
     }
 
@@ -37,50 +43,64 @@ void Heap::remove(int key) {
 
 int Heap::search(int key) const {
     int keyIndex = KEY_NOT_FOUND;
+
+    // Call search algorithm
     findKey(key, 0, keyIndex);
     return keyIndex;
 }
 
 void Heap::findKey(int &key, int currIdx, int &keyIndex) const {
+    // Base case
     if (currIdx >= table.getSize() || keyIndex != KEY_NOT_FOUND) {
         return;
     }
     if (table[currIdx] == key) {
+        // key found
         keyIndex = currIdx;
-    }
-    else if (table[currIdx] > key) {
+    } else if (table[currIdx] > key) {
+        // Key could be located deeper in the heap
         findKey(key, this->right(currIdx), keyIndex);
         findKey(key, this->left(currIdx), keyIndex);
     }
+    // If key is greater than table[currIdx] stop looking deeper
 }
 
 void Heap::print(const std::ostream &ostr, int index = 0, int space = 0) const {
+    // Gap between levels
     const int COUNT = 8;
 
+    // Base case
     if (index >= table.getSize())
         return;
 
+    // Space for new heap's level
     space += COUNT;
 
+    // Process right subtree first
     print(ostr, this->right(index), space);
 
+    // Process current node
     std::cout << std::endl;
     for (int i = COUNT; i < space; i++)
         std::cout << " ";
     std::cout << table[index] << std::endl;
 
+    // Process left subtree
     print(ostr, this->left(index), space);
 }
 
 int Heap::parent(int index) const {
+    // floor(index / 2): index from 1
     return ((index + 1) >> 1) - 1;
 }
 
 int Heap::left(int index) const {
+    // 2 * index: index from 1
     return ((index + 1) << 1) - 1;
 }
 
 int Heap::right(int index) const {
+    // 2 * index + 1: index from 1
     return (index + 1) << 1;
 }
 
@@ -88,6 +108,8 @@ void Heap::heapify(int index) {
     int l = this->left(index);
     int r = this->right(index);
     int heapSize = table.getSize();
+
+    // Find the greatest node
     int largest = index;
     if (l < heapSize && table[l] > table[largest]) {
         largest = l;
@@ -95,6 +117,8 @@ void Heap::heapify(int index) {
     if (r < heapSize && table[r] > table[largest]) {
         largest = r;
     }
+
+    // Exchange nodes and continue
     if (largest != index) {
         this->swap(table[index], table[largest]);
         this->heapify(largest);
@@ -107,7 +131,7 @@ void Heap::swap(int &x, int &y) const {
     y = temp;
 }
 
-std::ostream& operator<<(std::ostream &ostr, const Heap &heap) {
+std::ostream &operator<<(std::ostream &ostr, const Heap &heap) {
     ostr << std::string(40, '-') << std::endl;
     heap.print(ostr);
     ostr << std::string(40, '#') << std::endl;
--- data_structures/Heap.h
+++ data_structures/Heap.h
@@ -1,37 +1,57 @@
-//
-// Created by Jakub Plona PL on 22.03.2019.
-//
-
 #ifndef SDIZO_P1_HEAP_H
 #define SDIZO_P1_HEAP_H
 
 #include <string>
 #include <iostream>
+
 #include "Table.h"
 
 
 class Heap {
 public:
+    // Inserts key into heap
     void insert(int key);
+
+    // Removes key from heap
     void remove(int key);
+
+    // Searches for key in heap and returns key's index
+    // If key does not exists search() returns KEY_NOT_FOUND
     int search(int key) const;
+
+    // Draws heap in the console
     void print(const std::ostream &ostr, int index, int indent) const;
 
 private:
+    // Index returned by search() if key was not found
     static const int KEY_NOT_FOUND = -1;
 
+    // This heap is implemented as a table
     Table table;
 
+    // Parent node's index
     inline int parent(int index) const;
+
+    // Left child node's index
     inline int left(int index) const;
+
+    // Right child node's index
     inline int right(int index) const;
+
+    // Standard algorithm for restoring heap order in node
     void heapify(int index);
+
+    // Exchange values of two ints
     void swap(int &x, int &y) const;
+
+    // Performs a recursive search
     void findKey(int &key, int currIdx, int &keyIndex) const;
 
+    // For test purposes only
     friend class HeapTest;
 };
 
-std::ostream& operator<<(std::ostream &ostr, const Heap &heap);
+// Declared for convenience
+std::ostream &operator<<(std::ostream &ostr, const Heap &heap);
 
 #endif //SDIZO_P1_HEAP_H
